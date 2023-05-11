d3c7acbf8551f5b5ae2ffe9da0a4841d00807171
kubalfw <89728656+kubalfw@users.noreply.github.com>
2023-05-11 08:51:39 +0000

    remove useless icons

--- src/Icons.jsx
+++ src/Icons.jsx
@@ -1,33 +1,3 @@
-import LocationOnIcon from '@mui/icons-material/LocationOn'
-import TimelapseIcon from '@mui/icons-material/Timelapse'
-import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork'
-import MyLocationIcon from '@mui/icons-material/MyLocation'
-import ArticleIcon from '@mui/icons-material/Article'
-import TapAndPlayIcon from '@mui/icons-material/TapAndPlay'
-import CodeIcon from '@mui/icons-material/Code'
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline'
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline'
-import HealingIcon from '@mui/icons-material/Healing'
-import MoreTimeIcon from '@mui/icons-material/MoreTime'
-import SchoolIcon from '@mui/icons-material/School'
-import CallIcon from '@mui/icons-material/Call';
-import EmailIcon from '@mui/icons-material/Email';
-import AssignmentIcon from '@mui/icons-material/Assignment';
 
-export {
-	LocationOnIcon,
-	TimelapseIcon,
-	MapsHomeWorkIcon,
-	MyLocationIcon,
-	ArticleIcon,
-	TapAndPlayIcon,
-	CodeIcon,
-	ErrorOutlineIcon,
-	CheckCircleOutlineIcon,
-	HealingIcon,
-	MoreTimeIcon,
-	SchoolIcon,
-	CallIcon,
-	EmailIcon,
-	AssignmentIcon
-}
+
+export {}
