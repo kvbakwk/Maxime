916cbff9ce4be10c7c25382589a1cc1a05d74ad1
Jakub Kawka <jakubkawka2005@gmail.com>
2023-05-12 22:08:05 +0200

    Add table for initial costs. Fixes #1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,12 @@
 import '../styles/main.css'
+import data from '../data.json'
+
+import Initial from './Initial'
 
 const Main = () => {
-
-
     return (
         <div className="main">
-        
+            <Initial data={data} />
         </div>
     );
 };
